88ec6f54fc19af89abbcfbadb510d8a9dbbafa27
JP <85654561+jp524@users.noreply.github.com>
2022-07-09 12:14:29 -0400

    Add computerPlay()

--- game.js
+++ game.js
@@ -1,1 +1,10 @@
-console.log("hello");
+function computerPlay() {
+  let choice = Math.floor(Math.random() * 3);
+  if (choice === 0) {
+    return "Rock";
+  } else if (choice === 1) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
