12c24c27439a91e66cf9a08a05f95fa20a530d10
JP <85654561+jp524@users.noreply.github.com>
2022-07-09 12:36:45 -0400

    Add playRound()

--- game.js
+++ game.js
@@ -12,3 +12,22 @@ function computerPlay() {
 function capitalizeFirstLetter(input) {
   return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = capitalizeFirstLetter(playerSelection);
+
+  if (playerSelection === computerSelection) {
+    return "It's a tie!"
+  } else if ((playerSelection === "Rock" && computerSelection === "Scissors")
+    || (playerSelection === "Paper" && computerSelection === "Rock")
+    || (playerSelection === "Scissors" && computerSelection === "Paper")) {
+    return `You win! ${playerSelection} beats ${computerSelection}`
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`
+  }
+}
+
+// Test playRound()
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
