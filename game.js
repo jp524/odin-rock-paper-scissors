4d69f385327df6a393a7a86f6d512cb59326820a
JP <85654561+jp524@users.noreply.github.com>
2022-07-15 20:08:33 -0400

    Add multi-round functionality

--- game.js
+++ game.js
@@ -9,10 +9,6 @@ function computerPlay() {
   }
 }
 
-function capitalizeFirstLetter(input) {
-  return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
-}
-
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     results.textContent = "It's a tie!";
@@ -28,36 +24,30 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  // --- Previous code to play 5 rounds ---
-  // let scorePlayer = 0;
-  // let scoreComputer = 0;
-  // for (let i = 0; i < 5; i++) {
-  //   let playerSelection = capitalizeFirstLetter(prompt("Choose Rock, Paper or Scissors:"));
 
-  //   while (playerSelection !== "Rock" && playerSelection !== "Paper" && playerSelection !== "Scissors") {
-  //     console.log(`${playerSelection} is not a valid choice. Choose Rock, Paper or Scissors.`)
-  //     playerSelection = capitalizeFirstLetter(prompt("Choose Rock, Paper or Scissors:"));
-  //   }
-
-  //   let [pointPlayer, pointComputer] = playRound(playerSelection, computerPlay());
-  //   scorePlayer += pointPlayer;
-  //   scoreComputer += pointComputer;
-  // }
-  // console.log(`Score player: ${scorePlayer}`);
-  // console.log(`Score computer: ${scoreComputer}`);
-  // console.log(scorePlayer > scoreComputer ? "Player wins!" : "Computer wins!");
+function onButtonClick(e) {
+  if (scorePlayer < 5 && scoreComputer < 5) {
+    let [pointPlayer, pointComputer] = playRound(playerSelection = e.target.id, computerPlay());
+    scorePlayer += pointPlayer;
+    scoreComputer += pointComputer;
+    score.textContent = `Player ${scorePlayer} - ${scoreComputer} Computer`;
+    if (scorePlayer === 5) {
+      results.textContent = "You win!";
+    } else if (scoreComputer === 5) {
+      results.textContent = "The computer won!";
+    }
+  }
 }
 
 let playerSelection = "";
+let scorePlayer = 0;
+let scoreComputer = 0;
 
 const btn = document.querySelectorAll("button");
-btn.forEach(btn => btn.addEventListener("click", (e) => {
-  playerSelection = e.target.id;
-  playRound(playerSelection, computerPlay());
-}));
+btn.forEach(btn => btn.addEventListener("click", onButtonClick));
 
 const container = document.querySelector(".container");
 const results = document.createElement("p");
-
+const score = document.createElement("p");
 container.appendChild(results);
+container.appendChild(score);
